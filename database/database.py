b2d179113bc05e571005dd153b0c2881d95b381d
Abdulrahman Alguydi <abdulrahman.alguydi@student.manchester.ac.uk>
2026-07-11 06:50:03 +0300

    add default port

--- database/database.py
+++ database/database.py
@@ -11,7 +11,7 @@ def get_db_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
+            port=os.getenv('DB_PORT', 3306),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
             database=os.getenv('DB_NAME')
